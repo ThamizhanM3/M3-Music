6ee028ebddeb302ae6cba8881f4c205bdeb4c607
ThamizhanM3 <mitheleshv@gmail.com>
2026-05-31 21:41:31 +0530

    deployment v1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://m3music-backend-v1.onrender.com',
         changeOrigin: true,
       }
     }
